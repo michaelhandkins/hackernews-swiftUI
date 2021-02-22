069a62c81105825e687ee44078de1005df739125
Michael Handkins <michaelhandkins@gmail.com>
2021-02-22 17:08:46 -0600

    Create list view and navigation view with navbar title

--- H4X0R News (SwiftUI)/ContentView.swift
+++ H4X0R News (SwiftUI)/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("Hello, world!")
+                Text("Goodbye, World!")
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 11 Pro")
     }
 }
