edd2dd4bf12897a4f7aa8c322e358629b36af953
Michael Handkins <michaelhandkins@gmail.com>
2021-02-22 17:25:58 -0600

    Create Post struct which conforms to Identifiable protocol and populate List view with array of posts

--- H4X0R News (SwiftUI)/ContentView.swift
+++ H4X0R News (SwiftUI)/ContentView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            List {
-                Text("Hello, world!")
-                Text("Goodbye, World!")
+            List(posts) { post in
+                Text(post.title)
             }
             .navigationBarTitle("H4X0R News")
         }
@@ -25,3 +24,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView().previewDevice("iPhone 11 Pro")
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
