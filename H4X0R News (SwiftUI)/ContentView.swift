d59f76d7c0ed62e8220b4c810086e14e862e6b08
Michael Handkins <michaelhandkins@gmail.com>
2021-02-22 17:02:28 -0600

    Initial commit

---
+++ H4X0R News (SwiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News (SwiftUI)
+//
+//  Created by Michael Handkins on 2/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R News (SwiftUI)/H4X0R_News__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News__SwiftUI_App.swift
+//  H4X0R News (SwiftUI)
+//
+//  Created by Michael Handkins on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
